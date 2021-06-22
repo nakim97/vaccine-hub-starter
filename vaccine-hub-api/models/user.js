6fad860704eaaae6c768fdcbfcd2d83e432c21a6
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-06-22 11:51:20 -0700

    set up express application, authentication route, SQL script

---
+++ vaccine-hub-api/models/user.js
@@ -0,0 +1,14 @@
+const {UnauthorizedError } = require("../utils/errors")
+
+class User {
+    static async login(credentials) {
+
+        throw new UnauthorizedError("Invalid email/password combination")
+    }
+
+    static async register(credentials) {
+
+    }
+}
+
+module.exports = User
---
+++ vaccine-hub-api/routes/auth.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const router = express.Router()
+
+router.post("/login", async(req,res,next) => {
+    try{
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post("/register", async (req,res,next) => {
+    try{
+
+    } catch (err) {
+        next(err)
+    }
+})
+module.exports = router
